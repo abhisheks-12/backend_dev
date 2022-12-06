958c04981f6ea427aadde998868add2346fc7dfe
Abhishek Sankpal <abhishek.sankpal12@gmail.com>
2022-12-06 17:38:14 +0530

    :rocket:

--- task manager api/models/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const TaskScheme = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
-});
-
-const Tasks = mongoose.model("Task", TaskScheme);
-// module.exports = Tasks;
